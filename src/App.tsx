9e779f7a3be2729e57c896540029de94f12c7ec0
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-02-02 23:03:45 +0800

    Init tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css";
+// import "./App.css";
 
-export default function App() {
-  return <div className="bg-red-100">App</div>;
-}
+// export default function App() {
+//   return <div className="bg-red-100">App</div>;
+// }
